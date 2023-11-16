9128988984b9662e48e68cbf157b323a446e4445
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2023-11-16 23:16:48 +0530

    Update Voting2.sol

--- contracts/Voting2.sol
+++ contracts/Voting2.sol
@@ -1,5 +1,5 @@
-//SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0 < 0.9.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 contract Election {
 
@@ -21,40 +21,41 @@ contract Election {
 
     // DECIDE ADMINISTRATOR OF ELECTION
     address public administrator;
-    
+
     // DYNAMIC ARRAY TO STORE NO OF CANDIDATES
     Candidate[] public candidates;
 
-    // CANDIDATE REGISTRATION
+    // Mapping to store voter information
     mapping(address => Voter) public voters;
 
-    function registercandi(string memory _name, uint _number) public {
-        Candidate memory candi1 = Candidate({party: _name, numvotes: _number, voteCount:0});
-        candidates.push(candi1);
+    // CANDIDATE REGISTRATION
+    function registerCandidate(string memory _name, uint _number) public {
+        Candidate memory newCandidate = Candidate({party: _name, numvotes: _number, voteCount: 0});
+        candidates.push(newCandidate);
+
+        // Emit event for candidate registration
+        emit CandidateRegistered(candidates.length - 1, _name, _number);
     }
 
     // VOTER REGISTRATION
-    function registervoter( string memory _name, uint _number_id ) public{
-    Voter memory newVoter = Voter( _name, _number_id, true, false, 0 ) ;
-    voters[msg.sender] = newVoter ;
+    function registerVoter(string memory _name, uint _number_id) public {
+        require(!voters[msg.sender].isRegistered, "Voter is already registered.");
+        Voter memory newVoter = Voter(_name, _number_id, true, false, 0);
+        voters[msg.sender] = newVoter;
+
+        // Emit event for voter registration
+        emit VoterRegistered(msg.sender, _name, _number_id);
     }
 
     // Event to signal the registration of a new voter
     event VoterRegistered(address indexed voterAddress, string name, uint number_id);
 
-    // Function to retrieve voter information
-    function getvoter() public view returns( string memory, uint ){
-    Voter memory voter = voters[msg.sender] ;
-    return ( voter.name, voter.number_id ) ;
-    }
-    
-    // Mapping to associate candidate IDs with candidates
-    mapping(uint => Candidate) public candidateById;
-
     // Event to signal the registration of a new candidate
     event CandidateRegistered(uint indexed candidateId, string name, uint numvotes);
 
-    
+    // Mapping to associate candidate IDs with candidates
+    mapping(uint => Candidate) public candidateById;
+
     // Voting
     function vote(uint _candidateId) public {
         Voter storage voter = voters[msg.sender];
